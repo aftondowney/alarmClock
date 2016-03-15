1c4d7260e067e66f31385a6c5bd339d7eee8d490
Afton Downey and Michael Lor <aaike001@gmail.com>
2016-03-15 12:00:48 -0700

    add alarm clock functionality

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,3 +1,39 @@
+// exports.matchTime = function(alarm_time){
+//   for(var i = (moment().format("h:mm:ss")); i <= alarm_time; i++)
+//   {
+//
+//   }
+//
+// }
+
+
+
+
+
+
 $(document).ready(function(){
-  $('#time').text(moment().format("MMM Do YYYY, h:mm a"));
+
+  var alarm = null;
+
+  $('.set_alarm').submit(function(event) {
+    event.preventDefault();
+    var alarm_time = $("input#alarm_time").val();
+    var alarm_date = moment().format("MM DD YYYY");
+    var alarm_datetime = alarm_date + " " + alarm_time;
+    alarm = new Date(alarm_datetime);
+    console.log(alarm);
+  });
+
+
+
+  var refresh = function() {
+    console.log(alarm);
+    $('#time').text(moment().format("h:mm:ss"));
+    if(alarm && alarm <= moment()) {
+      alert("Time to get up!");
+      alarm = null;
+    }
+  }
+
+setInterval(refresh, 100);
 });
